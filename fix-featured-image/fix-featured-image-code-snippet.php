72119a6f8926c35980aa8f0263b102880bfde081
Ken Gagne <ken.gagne@automattic.com>
2023-11-17 23:26:13 +0100

    Replaces plugin & adds features
    The PHP file should've been a script but was instead a plugin. Fixed, and adds minimum widths and defaults/fallbacks.

--- fix-featured-image/fix-featured-image-code-snippet.php
+++ fix-featured-image/fix-featured-image-code-snippet.php
@@ -1,15 +1,15 @@
 <?php
+
 /**
-* Plugin Name: Fix Featured Images
-* Plugin URI: https://github.com/gamebits/easy-content-updates
-* Description: Grabs the first attached image for each post and sets it as the featured image.
-* Version: 1.0.0
-* Author: Newspack
-* Author URI: https://newspack.com/
-* License: GPL2
-* Text Domain: fix-featured-images
-* Domain Path: /languages/
-**/
+ * Fix featured images after a WXR import if needed
+ * by grabbing the first attached image for each post
+ * that has minimum 1200px width and setting it as the
+ * featured image. Otherwise, use fallback image.
+ */
+
+// Define fallback featured image ID & minimum width
+$fallback_featured_image_id = 0; // Defaults to using media attached to the post only; set to the non-zero post ID of a fallback media file to be used when no attachments are found.
+$image_anysize = 0; // When set to 0, a minimum width of 1200px is enforced; set to 1 to allow any size of featured image.
 
 // Get all posts.
 $posts = get_posts( [
@@ -27,15 +27,37 @@ foreach ( $posts as $post_id ) {
 			'post_type' => 'attachment',
 			'post_parent' => $post_id,
 			'fields' => 'ids',
+			'order' => 'ASC',
 		] );
 
 		if ( empty( $attachments ) ) {
 			WP_CLI::line( "No attachments found for post " . $post_id );
+			
+			// If fallback ID is not zero, assign it to the post.
+			if ( $fallback_featured_image_id != 0 ) {
+				WP_CLI::line( "Assigning fallback featured image " . $fallback_featured_image_id . " to post " . $post_id );
+				update_post_meta( $post_id, '_thumbnail_id', $fallback_featured_image_id );
+			}
+			
 			continue;
 		}
 
-		$featured_image_id = min( $attachments );
-		WP_CLI::line( "Found featured image " . $featured_image_id . " for post " . $post_id );
+		// Loop through attachments until one with sufficient width is found.
+		$featured_image_id = 0;
+		foreach ( $attachments as $attachment_id ) {
+			$metadata = wp_get_attachment_metadata( $attachment_id );
+			// If $image_anysize is 1, ignore width check.
+			if ( $image_anysize == 1 || ($metadata && $metadata['width'] >= 1200) ) {
+				$featured_image_id = $attachment_id;
+				break;
+			}
+		}
+
+		if ( $featured_image_id == 0 ) { // No suitable attachment was found
+			$featured_image_id = $fallback_featured_image_id;
+		}
+
+		WP_CLI::line( "Assigned featured image " . $featured_image_id . " for post " . $post_id );
 		update_post_meta( $post_id, '_thumbnail_id', $featured_image_id );
 	}
 }
